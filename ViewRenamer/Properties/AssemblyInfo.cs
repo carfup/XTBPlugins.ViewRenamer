9e81f7ae5f11f35ddbde1d0a07f084fcb5ad09fc
Clement Olivier <clement@carfup.com>
2024-01-10 15:51:49 +0100

    update appinsight version

--- ViewRenamer/Properties/AssemblyInfo.cs
+++ ViewRenamer/Properties/AssemblyInfo.cs
@@ -34,5 +34,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 
 [assembly: AssemblyVersion("1.2024.1.6")]
-[assembly: AssemblyFileVersion("1.2024.1.6 ")]
+[assembly: AssemblyFileVersion("1.2024.1.6")]
 
